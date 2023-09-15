1e9283ca79504e2681623ed75026d8fbe89766cb
MahmoudKhalid <mahmoud0122549@gmail.com>
2023-09-15 21:37:53 +0300

    setup schema, graphiql tool, dummy data

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const BookType = require("./schema/schema")
-
+const  schema  = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-     
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => console.log("Server is running"));
--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const books = [
+  { name: "cat", genre: "animals", id: "1" },
+  { name: "dog", genre: "animals", id: "2" },
+  { name: "bird", genre: "birds", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -11,4 +18,19 @@ const BookType = new GraphQLObjectType({
   }),
 });
 
-module.exports = BookType;
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parents, args) {
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
