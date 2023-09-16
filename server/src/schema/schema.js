49de79aa7aa2b855dac51f91dabfc9aaa7bfca5c
MahmoudKhalid <mahmoud0122549@gmail.com>
2023-09-16 08:31:34 +0300

    Author Type, relation between books and authors

--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -1,20 +1,47 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLFloat,
+  GraphQLID,
+} = graphql;
 
 const books = [
-  { name: "cat", genre: "animals", id: "1" },
-  { name: "dog", genre: "animals", id: "2" },
-  { name: "bird", genre: "birds", id: "3" },
+  { name: "cat", genre: "animals", id: "1", authorId: "1a" },
+  { name: "dog", genre: "animals", id: "2", authorId: "2a" },
+  { name: "bird", genre: "birds", id: "3", authorId: "3a" },
+];
+const authors = [
+  { name: "jonas", age: "44", id: "1a" },
+  { name: "Maximellian", age: "49", id: "2a" },
+  { name: "Mahmoud", age: "66", id: "3a" },
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        // console.log(parent);
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLFloat },
   }),
 });
 
@@ -23,9 +50,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parents, args) {
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parents, args) {
+        console.log(parents);
+        return _.find(authors, { id: args.id });
       },
     },
   },
